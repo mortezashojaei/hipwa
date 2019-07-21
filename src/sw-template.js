8d95b156391e24535aa11f6d6647694d4120766d
morteza shojaei` <mshojaei179@gmail.com>
2019-07-21 12:56:06 +0430

    add push notification listener

--- src/sw-template.js
+++ src/sw-template.js
@@ -29,6 +29,25 @@ if ('function' === typeof importScripts) {
         })
       );
   
+
+// Register event listener for the 'push' event.
+self.addEventListener('push', function(event) {
+  // Retrieve the textual payload from event.data (a PushMessageData object).
+  // Other formats are supported (ArrayBuffer, Blob, JSON), check out the documentation
+  // on https://developer.mozilla.org/en-US/docs/Web/API/PushMessageData.
+  const payload = event.data ? event.data.text() : 'no payload';
+
+  // Keep the service worker alive until the notification is created.
+  event.waitUntil(
+    // Show a notification with title 'ServiceWorker Cookbook' and use the payload
+    // as the body.
+    self.registration.showNotification('ServiceWorker Cookbook', {
+      body: payload,
+    })
+  );
+});
+
+
   
   } else {
       console.log('Workbox could not be loaded. No Offline support');
